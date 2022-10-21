f5805888ec5815a200001e91c971fae2171e410c
Luis Hiram Díaz <luishiram.ds@gmail.com>
2022-10-20 20:19:43 -0500

    Structured repository. Added packages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AboutPage, HomePage, ShopPage } from './pages/';
 
 function App() {
-  const [count, setCount] = useState(0)
+   return (
+      <div className='App'>
+         <h1>Alvaro Diaz</h1>
+         <hr />
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+         <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='about' element={<AboutPage />} />
+            <Route path='shop' element={<ShopPage />} />
+            <Route path='/*' element={<Navigate to='/' />} />
+         </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+   );
 }
 
-export default App
+export default App;
