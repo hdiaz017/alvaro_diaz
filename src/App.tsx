d0155c7b00604ec64ebec1467209e40ee904c366
Luis Hiram Díaz <luishiram.ds@gmail.com>
2022-10-22 23:24:40 -0500

    added basic navbar with mui and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { AboutPage, HomePage, ShopPage } from './pages/';
+import { NavBar } from './components/NavBar';
+import { AboutPage, GalleryPage, HomePage, ShopPage } from './pages/';
 
 function App() {
    return (
-      <div className='App'>
-         <h1>Alvaro Diaz</h1>
-         <hr />
+      <div>
+         <NavBar />
 
          <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='about' element={<AboutPage />} />
             <Route path='shop' element={<ShopPage />} />
+            <Route path='gallery' element={<GalleryPage />} />
             <Route path='/*' element={<Navigate to='/' />} />
          </Routes>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+   palette: {
+      mode: 'dark',
+   },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-   <BrowserRouter>
-      <React.StrictMode>
-         <App />
-      </React.StrictMode>
-   </BrowserRouter>,
+   <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+         <React.StrictMode>
+            <App />
+         </React.StrictMode>
+      </BrowserRouter>
+   </ThemeProvider>,
 );
