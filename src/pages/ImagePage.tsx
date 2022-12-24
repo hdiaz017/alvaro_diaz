3647c674368a42f7c691d034a8d482f6415feb8f
Luis Hiram Díaz <luishiram.ds@gmail.com>
2022-12-23 23:04:48 -0600

    Created ImagePage, added pagination to grid

--- src/pages/ImagePage.tsx
+++ src/pages/ImagePage.tsx
@@ -1,26 +1,40 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import { Drawings } from '../store/slices/drawings/drawingsSlice';
 import { useEffect } from 'react';
-import { Button, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 
 export const ImagePage = () => {
+   const navigate = useNavigate();
    const items = useSelector((state: RootState) => state.drawings.drawings);
+
    const { id } = useParams();
 
    useEffect(() => {
       window.scrollTo(0, 0);
    }, []);
-   const item: Drawings | undefined = items.find((i) => i.id == parseInt(id));
+   useEffect(() => {
+      if (items.length === 0) navigate('/');
+   }, []);
+   const item: Drawings | undefined = items.find((i) => {
+      if (id) {
+         return i.id == parseInt(id);
+      }
+   });
 
+   const priceFormat = new Intl.NumberFormat('es-MX', {
+      style: 'currency',
+      currency: 'MXN',
+   });
+   const currency = item?.price && priceFormat.format(item.price);
    return (
       <>
          <div
             style={{
                height: '100vh',
                display: 'flex',
-               justifyContent: 'space-around',
+               justifyContent: 'center',
                alignItems: 'center',
                margin: '0px 300px 0px 100px',
             }}
@@ -30,23 +44,33 @@ export const ImagePage = () => {
                alt={item?.name}
                style={{ height: '700px', borderRadius: '20px' }}
             />
-            <div>
-               <Typography>
-                  <strong>Título: </strong> {item?.name}
+            <Box ml={5}>
+               <Typography variant='h6' mt={4}>
+                  <li>
+                     <strong>Título: </strong> {item?.name}
+                  </li>
                </Typography>
-               <Typography>
-                  <strong>Precio: </strong> {item?.price}
+               <Typography variant='h6' mt={4}>
+                  <li>
+                     <strong>Precio: </strong> {currency} MXN
+                  </li>
                </Typography>
-               <Typography>
-                  <strong>Tamaño: </strong> 45x30cm
+               <Typography variant='h6' mt={4}>
+                  <li>
+                     <strong>Tamaño: </strong> 45x30cm
+                  </li>
                </Typography>
                <Button
-                  style={{ backgroundColor: '#2262cc', color: 'white' }}
+                  style={{
+                     backgroundColor: '#2262cc',
+                     color: 'white',
+                     marginTop: '30px',
+                  }}
                   variant='contained'
                >
                   Agregar al Carrito
                </Button>
-            </div>
+            </Box>
          </div>
       </>
    );
